10d1e8d4389a587efcdd97b8dc82ac8d8dc890d8
h4581 <h45818357@gmail.com>
2019-03-03 14:57:26 +0700

    edit.php form post

--- edit.php
+++ edit.php
@@ -6,11 +6,15 @@
     <?php
 
     if (isset($_SESSION['u_id'])) {
-      echo '<form class="signup-form edit-form" action="includes/edit.inc.php" method="post">
-              <input type="text" name="title" placeholder="Title" />
-              <textarea name="content" rows="8" cols="100"></textarea>
-              <button type="submit" name="submit">Post</button>
-            </form>';
+      ?>
+
+      <form class="signup-form edit-form" action="includes/edit.inc.php" method="post">
+        <input type="text" name="title" placeholder="Title" />
+        <textarea name="content" rows="8" cols="100"></textarea>
+        <button type="submit" name="submit">Post</button>
+      </form>
+
+      <?php
     } else {
       echo 'Sorry, you are not sign up yet, please sign up to post the article';
     }
