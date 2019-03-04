f84a533cd6c82a593bc8e1d1fc21b8fb263038d0
Hasbi Qohar <hasbiqohar@gmail.com>
2019-03-04 14:59:38 +0700

    logsystem login prepared statement

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -14,16 +14,25 @@ if (isset($_POST['submit'])) {
     exit();
   } else {
 
-    $sql = "SELECT * FROM users WHERE user_uid='$uid' or user_email='$uid'";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
+    // $sql = "SELECT * FROM users WHERE user_uid='$uid' or user_email='$uid'";
+    // $result = mysqli_query($conn, $sql);
+    // $resultCheck = mysqli_num_rows($result);
 
-    if ($resultCheck < 1) {
+    $sqlPreparedStmt = "SELECT * FROM users WHERE user_uid=? or user_email=?";
+    $stmt = mysqli_stmt_init($conn);
+
+    if(!mysqli_stmt_prepare($stmt, $sqlPreparedStmt)) {
+
+    // if ($resultCheck < 1) {
       header("Location: ../index.php?login=error");
       exit();
+
     } else {
 
+      mysqli_stmt_bind_param($stmt, "ss", $uid, $uid);
+      mysqli_stmt_execute($stmt);
 
+      $result = mysqli_stmt_get_result($stmt);
 
       if ($row = mysqli_fetch_assoc($result)) {
 
