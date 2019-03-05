1ca8aba98c0d04ed3a55162b77d1d8657d403068
Hasbi Qohar <hasbiqohar@gmail.com>
2019-03-05 13:48:16 +0700

    error n success message

--- signup.php
+++ signup.php
@@ -10,6 +10,58 @@
       <input type="password" name="pwd" placeholder="Password" />
       <button type="submit" name="submit">Sign up</button>
     </form>
+    <?php
+
+    $fullURL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+    if (strpos($fullURL, "signup=empty") == true) {
+      ?>
+
+      <p class="error-message">
+        You did not fill in all fields!
+      </p>
+
+      <?php
+      exit();
+    } elseif (strpos($fullURL, "signup=invalid") == true) {
+      ?>
+
+      <p class="error-message">
+        You used invalid characters, write in capitalize!
+      </p>
+
+      <?php
+      exit();
+    } elseif (strpos($fullURL, "signup=email") == true) {
+      ?>
+
+      <p class="error-message">
+        You used an invalid email!
+      </p>
+
+      <?php
+      exit();
+    } elseif (strpos($fullURL, "signup=usertaken") == true) {
+      ?>
+
+      <p class="error-message">
+        Username is taken! Try another name.
+      </p>
+
+      <?php
+      exit();
+    } elseif (strpos($fullURL, "signup=empty") == true) {
+      ?>
+
+      <p class="success-message">
+        You have been signed up!
+      </p>
+
+      <?php
+      exit();
+    }
+
+     ?>
   </section>
 
 <?php include 'footer.php' ?>
